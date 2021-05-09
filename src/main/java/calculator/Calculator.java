ec8012b34d747bf78c3d2976f7d6af7f452f7ca3
Kiarash Golzadeh <kiarash.gl@gmail.com>
2021-05-10 01:18:50 +0430

    Implement the power feature to pass its tests

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,4 +4,15 @@ public class Calculator {
     public int divide(int a, int b) {
         return a / b;
     }
+
+    public int power(int a, int b) {
+        if (b < 0)
+            throw new IllegalArgumentException();
+
+        int result = 1;
+        for (int i = 0; i < b; i++) {
+            result *= a;
+        }
+        return result;
+    }
 }

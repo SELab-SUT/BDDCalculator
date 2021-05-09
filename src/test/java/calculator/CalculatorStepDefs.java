979ef280b90865432b8320bc0bb2e5cabfc3784c
Kiarash Golzadeh <kiarash.gl@gmail.com>
2021-05-10 01:14:17 +0430

    Add power feature scenarios

---
+++ src/test/java/calculator/CalculatorStepDefs.java
@@ -0,0 +1,62 @@
+package calculator;
+
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+
+public class CalculatorStepDefs {
+    private Calculator calculator;
+    private int value1;
+    private int value2;
+    private char operation;
+    private int result;
+    private boolean isDivideByZeroThrown;
+    private boolean isIllegalArgumentThrown;
+
+    @Before
+    public void before() {
+        calculator = new Calculator();
+    }
+
+    @Given("^Two input values, (-?\\d+) and (-?\\d+), and an operation ([/^])$")
+    public void twoInputValuesAndAnOperation(int arg0, int arg1, char arg2) {
+        isDivideByZeroThrown = false;
+        value1 = arg0;
+        value2 = arg1;
+        operation = arg2;
+    }
+
+    @When("^I perform the operation$")
+    public void iPerformTheOperation() {
+        if (operation == '/') {
+            try {
+                result = calculator.divide(value1, value2);
+            } catch (ArithmeticException e) {
+                isDivideByZeroThrown = true;
+            }
+        } else {
+            try {
+                result = calculator.power(value1, value2);
+            } catch (IllegalArgumentException e) {
+                isIllegalArgumentThrown = true;
+            }
+        }
+    }
+
+    @Then("^I expect the result (-?\\d+)$")
+    public void iExpectTheResult(int arg0) {
+        Assert.assertEquals(arg0, result);
+    }
+
+    @Then("^I expect the divide by zero exception$")
+    public void iExpectTheDivideByZeroException() {
+        Assert.assertTrue(isDivideByZeroThrown);
+    }
+
+    @Then("^I expect the illegal argument exception$")
+    public void iExpectTheIllegalArgumentException() {
+        Assert.assertTrue(isIllegalArgumentThrown);
+    }
+}
---
+++ src/test/resources/features/calculator.feature
@@ -0,0 +1,37 @@
+@tag
+Feature: Calculator
+
+  Scenario: divide two numbers
+    Given Two input values, 4 and 2, and an operation /
+    When I perform the operation
+    Then I expect the result 2
+
+  Scenario: divide by zero
+    Given Two input values, -7 and 0, and an operation /
+    When I perform the operation
+    Then I expect the divide by zero exception
+
+  Scenario: raise a number to a power
+    Given Two input values, 3 and 4, and an operation ^
+    When I perform the operation
+    Then I expect the result 81
+
+  Scenario: raise a number to a negative power
+    Given Two input values, 5 and -4, and an operation ^
+    When I perform the operation
+    Then I expect the illegal argument exception
+
+  Scenario Outline: divide by zero
+    Given Two input values, <first> and <second>, and an operation <third>
+    When I perform the operation
+    Then I expect the result <result>
+    Examples:
+      | first | second | third  | result |
+      | 18    | 3      | /      | 6      |
+      | -320  | 16     | /      | -20    |
+      | -19   | -3     | /      | 6      |
+      | 21    | 185    | /      | 0      |
+      | -1    | 7      | ^      | -1     |
+      | 0     | 0      | ^      | 1      |
+      | 2     | 8      | ^      | 256    |
+      | -9    | 4      | ^      | 6561   |
